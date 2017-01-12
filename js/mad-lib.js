cb3f46c9a4208bb2e31f58b4a50c6cae4b6d82ad
Dylan Stackhouse <stackhouse.dylan@gmail.com>
2017-01-12 10:08:56 -0800

    add css and jquery functions for mad-lib.html

---
+++ js/mad-lib.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#blanks form").submit(function(event){
+    $(".groupOfPeople1").text($("input#groupOfPeople1").val());
+    $(".pluralNoun1").text($("input#pluralNoun1").val());
+    $(".groupOfPeople2").text($("input#groupOfPeople2").val());
+    $(".pluralNoun2").text($("input#pluralNoun2").val());
+    $(".emotion").text($("input#emotion").val());
+    $(".groupOfPeople1").text($("input#groupOfPeople1").val());
+
+    $("#story").show();
+    $(event).preventDefault();
+
+  });
+});
